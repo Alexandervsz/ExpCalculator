5879454112a0bcdbee640e14358ff9cc7a006415
Alexandervsz <alexander_5560@hotmail.com>
2023-01-13 09:31:32 +0100

    Clenaed up xptable a bit.

--- app/src/main/java/com/example/expcalculator/MainActivity.java
+++ app/src/main/java/com/example/expcalculator/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.expcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.util.Map.entry;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.LinkedHashMap;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Locale;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,126 +32,127 @@ public class MainActivity extends AppCompatActivity {
         textViewActionsRequired = findViewById(R.id.textViewRequiredActions);
         textViewTimeRequired = findViewById(R.id.textViewTimeRequired);
         textViewTimeRequiredText = findViewById(R.id.textViewTimeRequiredText);
-        LinkedHashMap<Integer, Integer> expTable = new LinkedHashMap<>();
-        expTable.put(2, 83);
-        expTable.put(3, 174);
-        expTable.put(4, 276);
-        expTable.put(5, 388);
-        expTable.put(6, 512);
-        expTable.put(7, 650);
-        expTable.put(8, 801);
-        expTable.put(9, 969);
-        expTable.put(10, 1154);
-        expTable.put(11, 1358);
-        expTable.put(12, 1584);
-        expTable.put(13, 1833);
-        expTable.put(14, 2107);
-        expTable.put(15, 2411);
-        expTable.put(16, 2746);
-        expTable.put(17, 3115);
-        expTable.put(18, 3523);
-        expTable.put(19, 3973);
-        expTable.put(20, 4470);
-        expTable.put(21, 5018);
-        expTable.put(22, 5624);
-        expTable.put(23, 6291);
-        expTable.put(24, 7028);
-        expTable.put(25, 7842);
-        expTable.put(26, 8740);
-        expTable.put(27, 9730);
-        expTable.put(28, 10824);
-        expTable.put(29, 12031);
-        expTable.put(30, 13363);
-        expTable.put(31, 14833);
-        expTable.put(32, 16456);
-        expTable.put(33, 18247);
-        expTable.put(34, 20224);
-        expTable.put(35, 22406);
-        expTable.put(36, 24815);
-        expTable.put(37, 27473);
-        expTable.put(38, 30408);
-        expTable.put(39, 33648);
-        expTable.put(40, 37224);
-        expTable.put(41, 41171);
-        expTable.put(42, 45529);
-        expTable.put(43, 50339);
-        expTable.put(44, 55649);
-        expTable.put(45, 61512);
-        expTable.put(46, 67983);
-        expTable.put(47, 75127);
-        expTable.put(48, 83014);
-        expTable.put(49, 91721);
-        expTable.put(50, 101333);
-        expTable.put(51, 111945);
-        expTable.put(52, 123660);
-        expTable.put(53, 136594);
-        expTable.put(54, 150872);
-        expTable.put(55, 166636);
-        expTable.put(56, 184040);
-        expTable.put(57, 203254);
-        expTable.put(58, 224466);
-        expTable.put(59, 247886);
-        expTable.put(60, 273742);
-        expTable.put(61, 302288);
-        expTable.put(62, 333804);
-        expTable.put(63, 368599);
-        expTable.put(64, 407015);
-        expTable.put(65, 449428);
-        expTable.put(66, 496254);
-        expTable.put(67, 547953);
-        expTable.put(68, 605032);
-        expTable.put(69, 668051);
-        expTable.put(70, 737627);
-        expTable.put(71, 814445);
-        expTable.put(72, 899257);
-        expTable.put(73, 992895);
-        expTable.put(74, 1096278);
-        expTable.put(75, 1210421);
-        expTable.put(76, 1336443);
-        expTable.put(77, 1475581);
-        expTable.put(78, 1629200);
-        expTable.put(79, 1798808);
-        expTable.put(80, 1986068);
-        expTable.put(81, 2192818);
-        expTable.put(82, 2421087);
-        expTable.put(83, 2673114);
-        expTable.put(84, 2951373);
-        expTable.put(85, 3258594);
-        expTable.put(86, 3597792);
-        expTable.put(87, 3972294);
-        expTable.put(88, 4385776);
-        expTable.put(89, 4842295);
-        expTable.put(90, 5346332);
-        expTable.put(91, 5902831);
-        expTable.put(92, 6517253);
-        expTable.put(93, 7195629);
-        expTable.put(94, 7944614);
-        expTable.put(95, 8771558);
-        expTable.put(96, 9684577);
-        expTable.put(97, 10692629);
-        expTable.put(98, 11805606);
-        expTable.put(99, 13034431);
-        expTable.put(100, 14391160);
-        expTable.put(101, 15889109);
-        expTable.put(102, 17542976);
-        expTable.put(103, 19368992);
-        expTable.put(104, 21385073);
-        expTable.put(105, 23611006);
-        expTable.put(106, 26068632);
-        expTable.put(107, 28782069);
-        expTable.put(108, 31777943);
-        expTable.put(109, 35085654);
-        expTable.put(110, 38737661);
-        expTable.put(111, 42769801);
-        expTable.put(112, 47221641);
-        expTable.put(113, 52136869);
-        expTable.put(114, 57563718);
-        expTable.put(115, 63555443);
-        expTable.put(116, 70170840);
-        expTable.put(117, 77474828);
-        expTable.put(118, 85539082);
-        expTable.put(119, 94442737);
-        expTable.put(120, 104273167);
+        Map<Integer, Integer> expTable = Map.<Integer, Integer>ofEntries(
+                entry(2, 83),
+                entry(3, 174),
+                entry(4, 276),
+                entry(5, 388),
+                entry(6, 512),
+                entry(7, 650),
+                entry(8, 801),
+                entry(9, 969),
+                entry(10, 1154),
+                entry(11, 1358),
+                entry(12, 1584),
+                entry(13, 1833),
+                entry(14, 2107),
+                entry(15, 2411),
+                entry(16, 2746),
+                entry(17, 3115),
+                entry(18, 3523),
+                entry(19, 3973),
+                entry(20, 4470),
+                entry(21, 5018),
+                entry(22, 5624),
+                entry(23, 6291),
+                entry(24, 7028),
+                entry(25, 7842),
+                entry(26, 8740),
+                entry(27, 9730),
+                entry(28, 10824),
+                entry(29, 12031),
+                entry(30, 13363),
+                entry(31, 14833),
+                entry(32, 16456),
+                entry(33, 18247),
+                entry(34, 20224),
+                entry(35, 22406),
+                entry(36, 24815),
+                entry(37, 27473),
+                entry(38, 30408),
+                entry(39, 33648),
+                entry(40, 37224),
+                entry(41, 41171),
+                entry(42, 45529),
+                entry(43, 50339),
+                entry(44, 55649),
+                entry(45, 61512),
+                entry(46, 67983),
+                entry(47, 75127),
+                entry(48, 83014),
+                entry(49, 91721),
+                entry(50, 101333),
+                entry(51, 111945),
+                entry(52, 123660),
+                entry(53, 136594),
+                entry(54, 150872),
+                entry(55, 166636),
+                entry(56, 184040),
+                entry(57, 203254),
+                entry(58, 224466),
+                entry(59, 247886),
+                entry(60, 273742),
+                entry(61, 302288),
+                entry(62, 333804),
+                entry(63, 368599),
+                entry(64, 407015),
+                entry(65, 449428),
+                entry(66, 496254),
+                entry(67, 547953),
+                entry(68, 605032),
+                entry(69, 668051),
+                entry(70, 737627),
+                entry(71, 814445),
+                entry(72, 899257),
+                entry(73, 992895),
+                entry(74, 1096278),
+                entry(75, 1210421),
+                entry(76, 1336443),
+                entry(77, 1475581),
+                entry(78, 1629200),
+                entry(79, 1798808),
+                entry(80, 1986068),
+                entry(81, 2192818),
+                entry(82, 2421087),
+                entry(83, 2673114),
+                entry(84, 2951373),
+                entry(85, 3258594),
+                entry(86, 3597792),
+                entry(87, 3972294),
+                entry(88, 4385776),
+                entry(89, 4842295),
+                entry(90, 5346332),
+                entry(91, 5902831),
+                entry(92, 6517253),
+                entry(93, 7195629),
+                entry(94, 7944614),
+                entry(95, 8771558),
+                entry(96, 9684577),
+                entry(97, 10692629),
+                entry(98, 11805606),
+                entry(99, 13034431),
+                entry(100, 14391160),
+                entry(101, 15889109),
+                entry(102, 17542976),
+                entry(103, 19368992),
+                entry(104, 21385073),
+                entry(105, 23611006),
+                entry(106, 26068632),
+                entry(107, 28782069),
+                entry(108, 31777943),
+                entry(109, 35085654),
+                entry(110, 38737661),
+                entry(111, 42769801),
+                entry(112, 47221641),
+                entry(113, 52136869),
+                entry(114, 57563718),
+                entry(115, 63555443),
+                entry(116, 70170840),
+                entry(117, 77474828),
+                entry(118, 85539082),
+                entry(119, 94442737),
+                entry(120, 104273167)
+        );
         Button button = findViewById(R.id.buttonCalculate);
         button.setOnClickListener(view -> {
             if (textViewTargetLevel.getText().toString().equals("")) {
@@ -181,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             if (expTable.get(targetLevel) != null) {
-                @SuppressWarnings("ConstantConditions") float requiredExperience = expTable.get(targetLevel) - currentXp;
+                float requiredExperience = expTable.get(targetLevel) - currentXp;
                 if (requiredExperience < 0) {
                     SetError(this.getResources().getString(R.string.i_xp));
                     return;
